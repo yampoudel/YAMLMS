d13685e3a60dc75766406dd6226b17bc7db2840b
yampoudel <rahulpaudel2015@outlook.com>
2026-03-31 11:44:44 +1000

    Adding user factory and schema refinements

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,15 +32,15 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'firstname' => ['required', 'string', 'max:255'],
-            'lastname' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'firstname' => $request->firstname,
-            'lastname' => $request->lastname,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'role' => 'Learner',
--- resources/views/admin/user/listusers.blade.php
+++ resources/views/admin/user/listusers.blade.php
@@ -2,52 +2,40 @@
 
 <body>
     <h1>List of users:</h2>
-        <table border="1" cellpadding="8" cellspacing="0">
+        <table border="2" cellpadding="8" cellspacing="0">
             <thead>
                 <tr>
-                    <th>ID</th>
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Role</th>
                     <th>Login</th>
                     <th>Email</th>
                     <th>Status</th>
-                    <th>Birthdate</th>
-                    <th>Phone</th>
-                    <th>Mobile</th>
                     <th>Country</th>
                     <th>City</th>
                     <th>Postcode</th>
                     <th>Suburb</th>
                     <th>Join Date</th>
                     <th>Last Login</th>
-                    <th>Created At</th>
-                    <th>Updated At</th>
-                    <th>Actions</th>
+                    <th colspan="2">Actions</th>
                 </tr>
             </thead>
 
             <tbody>
                 @foreach ($users as $user)
                     <tr>
-                        <td> {{ $user['id'] }}</td>
-                        <td> {{ $user['firstname'] }}</td>
-                        <td> {{ $user['lastname'] }}</td>
-                        <td> {{ $user['role'] }}</td>
-                        <td> {{ $user['login'] }}</td>
-                        <td> {{ $user['email'] }}</td>
-                        <td> {{ $user['status'] }}</td>
-                        <td> {{ $user['birthdate'] }}</td>
-                        <td> {{ $user['phone'] }}</td>
-                        <td> {{ $user['mobile'] }}</td>
-                        <td> {{ $user['country'] }}</td>
-                        <td> {{ $user['city'] }}</td>
-                        <td> {{ $user['postcode'] }}</td>
-                        <td> {{ $user['suburb'] }}</td>
-                        <td> {{ $user['joindate'] }}</td>
-                        <td> {{ $user['lastlogin'] }}</td>
-                        <td> {{ $user['created_at'] }}</td>
-                        <td> {{ $user['updated_at'] }}</td>
+                        <td>{{ $user->first_name }}</td>
+                        <td>{{ $user->last_name }}</td>
+                        <td>{{ $user->role }}</td>
+                        <td>{{ $user->login }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->status }}</td>
+                        <td>{{ $user->country }}</td>
+                        <td>{{ $user->city }}</td>
+                        <td>{{ $user->postcode }}</td>
+                        <td>{{ $user->suburb }}</td>
+                        <td>{{ $user->join_date }}</td>
+                        <td>{{ $user->last_login }}</td>
                         <td><button>Edit</button></td>
                         <td><button>Delete</button></td>
                     </tr>
